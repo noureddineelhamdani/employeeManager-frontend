8a70d9c652d70f47a021317783f24861368fd4e1
Noureddine <noureddineelhamdani@gmail.com>
2022-09-23 01:02:31 +0200

    employeeManager_frontend

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'productmanagerapp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('productmanagerapp');
+    expect(app['title']).toEqual('productmanagerapp');
   });
 
   it('should render title', () => {
